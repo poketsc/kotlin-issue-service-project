5520ef23d8bc701eec44b6035d6c351f7ecdd5a1
seungchan baek <poketsc@gmail.com>
2023-06-27 22:06:40 +0900

    코멘트 삭제 api 개발

--- issue-service/src/main/kotlin/project/issueservice/service/CommentService.kt
+++ issue-service/src/main/kotlin/project/issueservice/service/CommentService.kt
@@ -40,4 +40,12 @@ class CommentService (
             commentRepository.save(this).toResponse()
         }
     }
+
+    @Transactional
+    fun delete(issueId: Long, id: Long, userId: Long) {
+        val issue = issueRepository.findByIdOrNull(issueId) ?: throw NotFoundException("이슈가 존재하지 않습니다.")
+        commentRepository.findByIdAndUserId(id, userId)?.let {
+            issue.comments.remove(it)
+        }
+    }
 }
--- issue-service/src/main/kotlin/project/issueservice/web/CommentController.kt
+++ issue-service/src/main/kotlin/project/issueservice/web/CommentController.kt
@@ -1,10 +1,13 @@
 package project.issueservice.web
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import project.issueservice.config.AuthUser
 import project.issueservice.model.CommentRequest
@@ -32,4 +35,14 @@ class CommentController (
         @PathVariable id: Long,
         @RequestBody request: CommentRequest
     ) = commentService.edit(id, authUser.userId, request)
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(
+        authUser: AuthUser,
+        @PathVariable issueId: Long,
+        @PathVariable id: Long,
+    ) {
+        commentService.delete(issueId, id, authUser.userId)
+    }
 }
